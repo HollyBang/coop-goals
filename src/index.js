a48939c951b6104740d398eba8135e9d5334bd66
Andrey Barchuk <ndreybar@gmail.com>
2017-09-08 12:12:30 +0300

    added few simple component and react router 4

--- src/index.js
+++ src/index.js
@@ -1,8 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import App from './components/App';
+import SignIn from './components/SignIn';
+import SignUp from './components/SignUp';
+
+const MyApp = () => (
+  <MuiThemeProvider>
+    <Router>
+      <div>
+        <Route exact path="/" component={App} />
+        <Route path="/signin" component={SignIn} />
+        <Route path="/signup" component={SignUp} />
+      </div>
+    </Router>
+  </MuiThemeProvider>
+);
+
+ReactDOM.render(
+  <MyApp />,
+  document.getElementById('root')
+);
+
