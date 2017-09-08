ad2946003ab18ef8b9c1135cd3cc64bdc8cdd327
Andrey Barchuk <ndreybar@gmail.com>
2017-09-08 19:01:50 +0300

    added firebase auth

--- src/components/SignUp/index.jsx
+++ src/components/SignUp/index.jsx
@@ -1,10 +1,43 @@
 import React, { Component } from 'react';
 
+import { firebaseApp } from './../../firebase.js';
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
+
 class SignUp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: ''
+    }
+  }
+
+  signUp() {
+    console.log('this.state', this.state);
+    const { email, password } = this.state;
+    firebaseApp.auth().createUserWithEmailAndPassword(email, password)
+    .catch(error => {
+      console.log('error', error);
+    })
+  }
+
   render() {
     return (
       <div>
-        SignUp
+        <h2>Sign Up</h2>
+        <TextField
+          hintText="email"
+          floatingLabelText="Floating Label Text"
+          onChange={event => this.setState({ email: event.target.value })}
+        /><br />
+        <TextField
+          hintText="Password Field"
+          floatingLabelText="Password"
+          type="password"
+          onChange={event => this.setState({ password: event.target.value })}
+        /><br />
+        <RaisedButton label="Sign Up" primary={true} onClick={() => this.signUp()} />
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -7,9 +7,19 @@ import {
   Link
 } from 'react-router-dom';
 
+import { firebaseApp } from './firebase';
+
 import App from './components/App';
 import SignIn from './components/SignIn';
 import SignUp from './components/SignUp';
+
+firebaseApp.auth().onAuthStateChanged(user => {
+  if(user) {
+    console.log('user has signed in or up', user);
+  } else {
+    console.log('user has signed out or still needs to sign up');
+  }
+})
 
 const MyApp = () => (
   <MuiThemeProvider>
