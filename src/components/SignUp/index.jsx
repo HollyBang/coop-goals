83a876f0bb8a8a60709c975edcae0ffc9058a2da
Andrey Bar <mazafakand@gmail.com>
2017-09-10 14:51:35 +0300

    added modal window when sign up  error occured

--- src/components/SignUp/index.jsx
+++ src/components/SignUp/index.jsx
@@ -3,15 +3,25 @@ import React, { Component } from 'react';
 import { firebaseApp } from './../../firebase.js';
 import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
+import Dialog from 'material-ui/Dialog';
+import FlatButton from 'material-ui/FlatButton';
 
 class SignUp extends Component {
   constructor(props) {
     super(props);
     this.state = {
       email: '',
-      password: ''
+      password: '',
+      error: {
+        message: ''
+      },
+      open: false
     }
   }
+
+  handleClose = () => {
+    this.setState({open: false});
+  };
 
   signUp() {
     console.log('this.state', this.state);
@@ -19,16 +29,27 @@ class SignUp extends Component {
     firebaseApp.auth().createUserWithEmailAndPassword(email, password)
     .catch(error => {
       console.log('error', error);
+      this.setState({
+        error,
+        open: true
+      });
     })
   }
 
   render() {
+    const actions = [
+      <FlatButton
+        label="OK"
+        primary={true}
+        onClick={this.handleClose}
+      />
+    ];
     return (
       <div>
         <h2>Sign Up</h2>
         <TextField
           hintText="email"
-          floatingLabelText="Floating Label Text"
+          floatingLabelText="Write email here!"
           onChange={event => this.setState({ email: event.target.value })}
         /><br />
         <TextField
@@ -38,6 +59,14 @@ class SignUp extends Component {
           onChange={event => this.setState({ password: event.target.value })}
         /><br />
         <RaisedButton label="Sign Up" primary={true} onClick={() => this.signUp()} />
+        <Dialog
+          actions={actions}
+          modal={false}
+          open={this.state.open}
+          onRequestClose={this.handleClose}
+        >
+          {this.state.error.message}
+        </Dialog>
       </div>
     );
   }
