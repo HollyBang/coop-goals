e89a1fe4932bbf1f782c00f15e9839b0a9529d22
Andrey Barchuk <ndreybar@gmail.com>
2017-09-12 11:49:52 +0300

    added history for rr4 and sign out component

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
+import { firebaseApp } from './../../firebase.js';
+
+import RaisedButton from 'material-ui/RaisedButton';
+
 
 class App extends Component {
+
+
+  signOut() {
+    firebaseApp.auth().signOut();
+  }
+
   render() {
     return (
       <div>
         hello
+        <br />
+        <RaisedButton
+        label="Sign Out"
+        secondary={true}
+        onClick={() => this.signOut()} />
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -2,9 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import {
-  BrowserRouter as Router,
-  Route
+  Router,
+  Route,
+  Redirect,
+  withRouter,
+  Link
 } from 'react-router-dom';
+import history from './history';
 
 import { firebaseApp } from './firebase';
 
@@ -12,18 +16,30 @@ import App from './components/App';
 import SignIn from './components/SignIn';
 import SignUp from './components/SignUp';
 
-firebaseApp.auth().onAuthStateChanged(user => {
-  if(user) {
-    console.log('user has signed in or up', user);
-  } else {
-    console.log('user has signed out or still needs to sign up');
-  }
-})
+
+
+  firebaseApp.auth().onAuthStateChanged(user => {
+    if (user) {
+      console.log('user has signed in or up', user);
+      history.push('/');
+    } else {
+      console.log('user has signed out or still needs to sign up');
+      history.replace('/signin');
+    }
+  });
+
+
+
 
 const MyApp = () => (
   <MuiThemeProvider>
-    <Router>
+    <Router history={history}>
       <div>
+        <ul>
+          <li><Link to="/">Public Page</Link></li>
+          <li><Link to="/signin">sign in</Link></li>
+          <li><Link to="/signup">sign up</Link></li>
+        </ul>
         <Route exact path="/" component={App} />
         <Route path="/signin" component={SignIn} />
         <Route path="/signup" component={SignUp} />
