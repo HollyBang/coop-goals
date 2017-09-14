150b52b9e2555114185f68f078e80b6c1fcc8463
Andrey Barchuk <ndreybar@gmail.com>
2017-09-14 13:42:46 +0300

    added goals list

--- src/components/App/GoalList.jsx
+++ src/components/App/GoalList.jsx
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
 import { goalRef } from './../../firebase';
+import { setGoals } from './../../actions';
+
+
 class GoalList extends Component {
 
   componentWillMount() {
     goalRef.on('value', snapshot => {
+      let goals = [];
       snapshot.forEach(goal => {
+        const { email, title } = goal.val();
         let goalObj = goal.val();
-        console.log('goalObj', goalObj);
+        goals.push(goalObj);
       })
+      this.props.setGoals(goals);
     })
   }
 
@@ -20,4 +28,11 @@ class GoalList extends Component {
   }
 }
 
-export default GoalList;
+function mapStateToProps(state) {
+  const { goals } = state;
+  return {
+    goals
+  }
+}
+
+export default connect(mapStateToProps, { setGoals })(GoalList);
