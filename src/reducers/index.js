3f49041d4c785b6bbf16da9587ea35ae174977d6
Andrey Barchuk <ndreybar@gmail.com>
2017-09-15 12:45:40 +0300

    complete redux logic

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,8 +2,10 @@ import { combineReducers } from 'redux';
 
 import user from './reducer_user';
 import goals from './reducer_goals';
+import completeGoals from './reducer_completeGoals';
 
 export default combineReducers({
   user,
-  goals
+  goals,
+  completeGoals
 })
