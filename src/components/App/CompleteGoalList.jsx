713987d07cbcbf0c4c4cd3335f5a7ce1753eb384
Andrey Barchuk <ndreybar@gmail.com>
2017-09-17 22:33:22 +0300

    added complete goal list

--- src/components/App/CompleteGoalList.jsx
+++ src/components/App/CompleteGoalList.jsx
@@ -1,5 +1,12 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import { setCompleted } from './../../actions';
 import { completeGoalRef } from './../../firebase.js';
+
+import { List, ListItem } from 'material-ui/List';
+import RaisedButton from 'material-ui/RaisedButton';
+import AssigIn from 'material-ui/svg-icons/action/assignment-turned-in';
 
 class CompleteGoalList extends Component {
   componentDidMount() {
@@ -7,19 +14,45 @@ class CompleteGoalList extends Component {
       let completeGoals = [];
       snapshot.forEach(completeGoal => {
         const { email, title } = completeGoal.val();
-        completeGoals.push({email, title});
+        completeGoals.push({ email, title });
       })
-      
+      this.props.setCompleted(completeGoals);
     })
+  }
+
+  clearAll() {
+    completeGoalRef.set([]);
   }
 
   render() {
     return (
-      <div>
-        
-      </div>
+      <List>
+        {
+          this.props.completeGoals.map((completeGoal, i) => {
+            const { title, email } = completeGoal;
+            return (
+              <ListItem
+                key={i}
+                leftIcon={<AssigIn />}
+                primaryText={title}
+                secondaryText={`Completed by ${email}`}
+              />
+            )
+          })
+        }
+        <RaisedButton
+          label="Clear All"
+          secondary={true}
+          onClick={() => this.clearAll()} />
+      </List>
     );
   }
 }
 
-export default CompleteGoalList;
+function mapStateToProps(state) {
+  const { completeGoals } = state;
+  return {
+    completeGoals
+  }
+}
+export default connect(mapStateToProps, { setCompleted })(CompleteGoalList);
